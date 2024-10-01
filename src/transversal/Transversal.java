71267094e4a896a44d48b4219ea6bed525cc8a52
KJC-dev11 <kevinjofre1111@gmail.com>
2024-10-01 19:21:00 -0300

    Proyecto Transversal

---
+++ src/transversal/Transversal.java
@@ -0,0 +1,17 @@
+
+package transversal;
+
+/**
+ *
+ * @author MOON
+ */
+public class Transversal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
